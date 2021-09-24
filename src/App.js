b2b884ee37f6a6b1951041b6b826b00ad383457d
Ken Hudnell <khudnell252@gmail.com>
2021-09-23 21:19:54 -0400

    tailwind initialization

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import {BiTrash} from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
-    <BiTrash /><h1> Empty Trash</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+    <h1 className="text-5xl"> <BiTrash className="inline-block text-red-400 align-top"/>Your Appointments</h1>
      
     </div>
   );
