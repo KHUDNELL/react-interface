5b3fdb1128180b73714d6c5e26eaf2563749d6a3
Ken Hudnell <khudnell252@gmail.com>
2021-09-26 12:04:28 -0400

    added search functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ import AppointmentInfo from "./components/AppointmentInfo";
 function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  );
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -23,12 +34,14 @@ function App() {
   }, [fetchData]);
   return (
     <div className="App container mx-auto mt-3 font-thin">
-    <h1 className="text-5xl mb-3"> <BiCalendar className="inline-block text-red-400 align-top"/>Your Appointments</h1>
+    <h1 className="text-4xl mb-3"> <BiCalendar className="inline-block text-red-400 align-top"/>Your Appointments</h1>
      <AddAppointment />
-     <Search />
+     <Search query={query}
+       onQueryChange={myQuery => setQuery(myQuery)}
+     />
 
      <ul className="divide-y divide-gray-200">
-       {appointmentList
+       {filteredAppointments
        .map(appointment => (
           <AppointmentInfo key={appointment.id}
           appointment = {appointment}
